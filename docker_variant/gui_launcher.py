acfbb9869f4822d43f0c4aa1416edb751c461074
Nidhish Kumar <snidhishkumar2003@gmail.com>
2026-02-26 13:28:18 +0530

    GUI bug fixes

--- docker_variant/gui_launcher.py
+++ docker_variant/gui_launcher.py
@@ -29,8 +29,10 @@ class PipelineGUI:
             lambda e: self.canvas.configure(scrollregion=self.canvas.bbox("all"))
         )
 
-        self.canvas.create_window((0,0), window=self.scrollable_frame, anchor="nw")
+        self.window_id = self.canvas.create_window((0,0), window=self.scrollable_frame, anchor="nw")
         self.canvas.configure(yscrollcommand=self.scrollbar.set)
+        
+        self.canvas.bind("<Configure>", lambda e: self.canvas.itemconfig(self.window_id, width=e.width))
 
         self.canvas.pack(side="left", fill="both", expand=True)
         self.scrollbar.pack(side="right", fill="y")
